ef5d262d2ff3321c4712fef790325b5bf661828d
puplee <285985622@qq.com>
2016-11-15 17:09:50 +0800

    five

--- Application/Home/Controller/UserController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-    class UserController extends Controller
-    {
-        public function index(){
-        	
-        }
-    }
--- qwe.php
+++
@@ -1,2 +0,0 @@
-<?php
-	echo "qwe is ";
